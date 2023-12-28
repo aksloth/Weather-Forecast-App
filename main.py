4ef7e33a23a58fdb09a2fc4eb60c81979b3a8fa3
joshua <joshua.r.stroud@hotmail.com>
2023-12-28 13:33:43 -1000

    Day 32: Initial commit, add widgets

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Weather Forecast for the next 5 days")
+place = st.text_input("Place: ", placeholder="place here")
+days = st.slider(
+    "Forecast Days",
+    min_value=1,
+    max_value=5,
+    help="Select number of forecasted days")
+option = st.selectbox("Select data to view: ",
+                      ("Temperature", "Sky"))
+st.subheader(f"{option} for the next {days} days in {place}")
